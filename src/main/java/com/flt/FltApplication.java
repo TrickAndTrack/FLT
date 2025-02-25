dfcb36c1c16c7475d84cacbb1f748231fe61ebce
TrickAndTrack <nahushzilpilwar@gmail.com>
2025-02-25 20:08:09 +0530

    FLT Project commit

---
+++ src/main/java/com/flt/FltApplication.java
@@ -0,0 +1,13 @@
+package com.flt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FltApplication.class, args);
+	}
+
+}
